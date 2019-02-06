d592bfcf8c7ecd8c8f8358abc8612814b007f572
Maurício Oliveira <e810@itsector.pt>
2019-02-06 16:39:04 +0000

    Initial Commit

---
+++ RandomNumberGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomNumberGenerator
+//
+//  Created by Maurício Oliveira on 06/02/2019.
+//  Copyright © 2019 Maurício Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
