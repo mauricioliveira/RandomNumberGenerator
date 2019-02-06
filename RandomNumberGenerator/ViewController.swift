6fc1c014197763a5ec4433bbe3817b208fa1193e
Maurício Oliveira <e810@itsector.pt>
2019-02-06 17:21:01 +0000

    Initial Commit

--- RandomNumberGenerator/ViewController.swift
+++ RandomNumberGenerator/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func generateNumber(_ sender: Any) {
+        
+        let random = arc4random_uniform(11)
+        
+        result.text = String(random)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
